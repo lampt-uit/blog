aa0401f06c90fa44b14191c9a1a8b78471cf6071
Pham Tan Lam <lamlampnqn12@gmail.com>
2021-07-10 15:36:54 +0700

    Header + Footer

---
+++ client/src/components/global/Search.tsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+
+const Search = () => {
+	const [search, setSearch] = useState('');
+	return (
+		<div className='search w-100 position-relative pt-2'>
+			<input
+				type='text'
+				className='form-control me-2 w-100 '
+				value={search}
+				placeholder='Enter your search ...'
+				onChange={(e) => setSearch(e.target.value)}
+			/>
+		</div>
+	);
+};
+
+export default Search;
