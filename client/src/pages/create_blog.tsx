952af594c80ced46d728b5c26ddbdb0e2054d84c
Pham Tan Lam <lamlampnqn12@gmail.com>
2021-07-21 20:23:54 +0700

    FE - Create Blog (React Quill) uncomplete

---
+++ client/src/pages/create_blog.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import NotFound from '../components/global/NotFound';
+import CreateForm from '../components/cards/CreateForm';
+import CardHoriz from '../components/cards/CardHoriz';
+import ReactQuill from '../components/editor/ReactQuill';
+
+import { RootStore, IBlog } from '../utils/TypeScript';
+
+const CreateBlog = () => {
+	const dispatch = useDispatch();
+
+	const initState = {
+		user: '',
+		title: '',
+		content: '',
+		description: '',
+		thumbnail: '',
+		category: '',
+		createdAt: new Date().toISOString()
+	};
+
+	const [blog, setBlog] = useState<IBlog>(initState);
+	const [body, setBody] = useState('');
+	const { auth } = useSelector((state: RootStore) => state);
+
+	if (!auth.access_token) return <NotFound />;
+	return (
+		<div className='my-4 create_blog'>
+			<div className='row mt-4'>
+				<div className='col-md-6'>
+					<h5>Create</h5>
+					<CreateForm blog={blog} setBlog={setBlog} />
+				</div>
+				<div className='col-md-6'>
+					<h5>Preview</h5>
+					<CardHoriz blog={blog} />
+				</div>
+			</div>
+			<ReactQuill setBody={setBody} />
+
+			<button className='btn btn-dark mt-3 d-block mx-auto'>Create Post</button>
+		</div>
+	);
+};
+
+export default CreateBlog;
--- client/src/utils/ImageUpload.ts
+++ client/src/utils/ImageUpload.ts
@@ -1,11 +1,12 @@
 export const checkImage = (file: File) => {
+	const types = ['image/png', 'image/jpeg'];
 	let err = '';
 	if (!file) return (err = 'File does not exists.');
 
 	if (file.size > 1024 * 1024)
 		// >1mb
-		err = 'The largest image size is 1mb';
-
+		err = 'The largest image size is 1mb.';
+	if (!types.includes(file.type)) err = 'The image type is png / jpeg';
 	return err;
 };
 
