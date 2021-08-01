62201b853c0d1e29f85f899d0d8c6ab393795082
Pham Tan Lam <lamlampnqn12@gmail.com>
2021-08-01 10:52:43 +0700

    Get blogs by category + Pagination BE

--- server/controllers/blog.controller.ts
+++ server/controllers/blog.controller.ts
@@ -1,7 +1,16 @@
 import { Request, Response } from 'express';
+import mongoose from 'mongoose';
 
 import Blogs from '../models/blog.model';
 import { IReqAuth } from '../config/interface';
+
+const Pagination = (req: IReqAuth) => {
+	let page = Number(req.query.page) * 1 || 1;
+	let limit = Number(req.query.limit) * 1 || 4;
+	let skip = (page - 1) * limit;
+
+	return { page, limit, skip };
+};
 
 const blogController = {
 	createBlog: async (req: IReqAuth, res: Response) => {
@@ -89,6 +98,84 @@ const blogController = {
 			res.json(blogs);
 		} catch (error: any) {
 			return res.status(400).json({ msg: error.message });
+		}
+	},
+	getBlogsByCategory: async (req: Request, res: Response) => {
+		const { limit, skip } = Pagination(req);
+		try {
+			const Data = await Blogs.aggregate([
+				{
+					$facet: {
+						totalData: [
+							{
+								$match: {
+									category: mongoose.Types.ObjectId(req.params.category_id)
+								}
+							},
+							{
+								$lookup: {
+									from: 'users',
+									let: { user_id: '$user' },
+									pipeline: [
+										{
+											$match: { $expr: { $eq: ['$_id', '$$user_id'] } }
+										},
+										{
+											$project: { password: 0 }
+										}
+									],
+									as: 'user'
+								}
+							},
+							//user : array => object
+							{
+								$unwind: '$user'
+							},
+							{
+								$sort: { createdAt: -1 }
+							},
+							{
+								$skip: skip
+							},
+							{
+								$limit: limit
+							}
+						],
+						totalCount: [
+							{
+								$match: {
+									category: mongoose.Types.ObjectId(req.params.category_id)
+								}
+							},
+							{
+								$count: 'count'
+							}
+						]
+					}
+				},
+				{
+					$project: {
+						count: { $arrayElemAt: ['$totalCount.count', 0] },
+						totalData: 1
+					}
+				}
+			]);
+
+			const blogs = Data[0].totalData;
+			const count = Data[0].count;
+
+			//Pagination
+			let total = 0;
+
+			if (count % limit === 0) {
+				total = count / limit;
+			} else {
+				total = Math.floor(count / limit) + 1;
+			}
+
+			res.json({ blogs, total });
+		} catch (error) {
+			return res.status(500).json({ msg: error.message });
 		}
 	}
 };
--- server/routes/blog.router.ts
+++ server/routes/blog.router.ts
@@ -7,5 +7,6 @@ const router = express.Router();
 
 router.post('/blog', auth, blogController.createBlog);
 router.get('/home/blogs', blogController.getHomeBlogs);
+router.get('/blogs/:category_id', blogController.getBlogsByCategory);
 
 export default router;
